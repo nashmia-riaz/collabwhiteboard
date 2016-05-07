b701d663d944f89d9e8eb68f761583770f21c41e
Nashmia Riaz <nashmia-riaz@hotmail.com>
2016-05-07 17:31:38 +0500

    fixed width, removed resize function

--- board.js
+++ board.js
@@ -1,31 +1,58 @@
+/* PubNub */
+/* Draw on canvas */
 
 var canvas = document.getElementById('drawCanvas');
 var ctx = canvas.getContext('2d');
+var color = 'yellowgreen';
+   
+canvas.width = Math.min(document.documentElement.clientWidth, window.innerWidth || 300);
+canvas.height = Math.min(document.documentElement.clientHeight, window.innerHeight || 300);
 
-ctx.lineWidth = '3';
 
-canvas.addEventListener('mousedown', startDraw, false);
-canvas.addEventListener('mousemove', draw, false);
-canvas.addEventListener('mouseup', endDraw, false);
+ctx.strokeStyle = color;
+ctx.lineWidth = '30';
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
 
-// create a flag
-var isActive = false;
 
-// array to collect coordinates
-var plots = [];
+var isTouchSupported = 'ontouchstart' in window;
+var isPointerSupported = navigator.pointerEnabled;
+var isMSPointerSupported =  navigator.msPointerEnabled;
+  
+var downEvent = isTouchSupported ? 'touchstart' : (isPointerSupported ? 'pointerdown' : (isMSPointerSupported ? 'MSPointerDown' : 'mousedown'));
+var moveEvent = isTouchSupported ? 'touchmove' : (isPointerSupported ? 'pointermove' : (isMSPointerSupported ? 'MSPointerMove' : 'mousemove'));
+var upEvent = isTouchSupported ? 'touchend' : (isPointerSupported ? 'pointerup' : (isMSPointerSupported ? 'MSPointerUp' : 'mouseup'));
 
-function draw(e) {
-  if(!isActive) return;
+canvas.addEventListener(downEvent, startDraw, false);
+canvas.addEventListener(moveEvent, draw, false);
+canvas.addEventListener(upEvent, endDraw, false);
 
-  // cross-browser canvas coordinates
-  var x = e.offsetX || e.layerX - canvas.offsetLeft;
-  var y = e.offsetY || e.layerY - canvas.offsetTop;
+var channel = 'draw';
 
-  plots.push({x: x, y: y});
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-b0111032-2103-4c52-a628-037979ab2842',
+    subscribe_key: 'sub-c-298a1dac-0955-11e6-bbd9-02ee2ddab7fe',
+});
 
-  drawOnCanvas([1,0,0],plots);
-}
+pubnub.subscribe({
+  channel: channel,
+  callback: drawFromStream,
+  presence: function(m){
+    if(m.occupancy > 0){
+      document.getElementById('occupancy').textContent = m.occupancy;
+    }
+  }
+});
+
+function publish(data) {
+  pubnub.publish({
+    channel: channel,
+    message: data
+  });
+   }
+
 function drawOnCanvas(color, plots) {
+  ctx.strokeStyle=color;
   ctx.beginPath();
   ctx.moveTo(plots[0].x, plots[0].y);
 
@@ -34,24 +61,48 @@ function drawOnCanvas(color, plots) {
   }
   ctx.stroke();
 }
+
 function drawFromStream(message) {
-    if(!message) return;        
-
-    ctx.beginPath();
-    drawOnCanvas(message.plots);
+  if(!message || message.plots.length < 1) 
+    return;      
+  drawOnCanvas(message.color, message.plots);
 }
+
+var isActive = false;
+var plots = [];
+
+function draw(e) {
+  e.preventDefault();
+  if(!isActive) return;
+
+  var x = isTouchSupported ? (e.targetTouches[0].pageX - canvas.offsetLeft) : (e.offsetX || e.layerX - canvas.offsetLeft);
+  var y = isTouchSupported ? (e.targetTouches[0].pageY - canvas.offsetTop) : (e.offsetY || e.layerY - canvas.offsetTop);
+
+  plots.push({x: (x << 0), y: (y << 0)}); // round numbers for touch screens
+  
+  drawOnCanvas(color, plots);
+}
+  
+  
+  
+  
+    
 function startDraw(e) {
+  e.preventDefault();
+  
   isActive = true;
+
+}
+  
+function endDraw(e) {
+  e.preventDefault();  
+  isActive = false;
+  
+  publish({
+    color:color,
+    plots:plots
+  });
+
+  plots = [];
 }
 
-function endDraw(e) {
-  isActive = false;
-	 pubnub.publish({
-	    channel: channel,
-	    message: { 
-	        plots: plots // your array goes here
-	    } 
-	});
-  // empty the array
-  plots = [];
-} 
